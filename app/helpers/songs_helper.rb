4ffbb5c4c0d66dc730b0eb72d35795c702aebb21
Maddog164 <denise@henderson3.us>
2020-12-06 10:34:47 -0500

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,12 +1,20 @@
 module SongsHelper
 
-    def artist_id_field(song)
-        if song.artist.nil?
-          select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    # def artist_id_field(song)
+    #     if song.artist.nil?
+    #       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    #     else
+    #       hidden_field_tag "song[artist_id]", song.artist_id
+    #     end
+    #     # binding.pry
+    # end
+
+    def artist_select(artist, song)
+        if artist
+            artist.name
         else
-          hidden_field_tag "song[artist_id]", song.artist_id
+            select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
         end
-        # binding.pry
-      end
+    end
 
 end
